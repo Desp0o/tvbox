eca7ae084360e455e2f6802ba95b35ba15aaf33a
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-07-30 22:51:54 +0400

    device section

---
+++ src/pages/main/device/device.js
@@ -0,0 +1,62 @@
+import "./device.css"
+
+import box from "../../../components/images/box.png"
+import remote from "../../../components/images/remote.png"
+import hdmi from '../../../components/images/hdmi.png'
+import power from '../../../components/images/power.png'
+
+const hardwareArr = [
+    {
+        data: 'MYVIDEO TV BOX',
+        image: `${box}`
+    },
+
+    {
+        data: 'IR პულტი',
+        image: `${remote}`
+    },
+
+    {
+        data: 'კვების ბლოკი',
+        image: `${power}`,
+        class: 'device_img_block_power'
+    },
+
+    {
+        data: 'HDMI კაბელი',
+        image: `${hdmi}`
+    },
+
+]
+
+export default function Device() {
+
+    return(
+        <>
+            <div className="device">
+                <div className="device_inner">
+                    <div className="device_first_txt">
+                        <p className="device_title">მოწყობილობა</p>
+                        <p className="device_desc">MYVIDEO TV BOX არის მოწყობილობა, რომლის მეშვეობითაც ნებისმიერ ტელევიზორში შეგიძლიათ მიიღოთ myvideo.ge-ზე არსებული სერვისები: ქართული და უცხოური სატელევიზიო არხები გადახვევა-დაპაუზების ფუნქციით, ვიდეოები, ახალი ამბები და მრავალი სხვა.</p>
+                        <p className="device_desc">სერვისის გამართული მუშაობისათვის საჭიროა სტაბილური ინტერნეტს შეერთება.</p>
+                        <p className="device_desc">(სიჩქარე მინიმუმ 3 მეგაბიტი წამში)</p>
+                    </div>
+
+                    <div className="device_sec_txt">
+                        <div className="device_title">კომპლექტაცია</div>
+
+                        <div className="device_images">
+                            {hardwareArr.map((part, index) => {
+                                return(
+                                    <div className={part.class ? part.class : 'device_img_block'} data-title={part.data} key={index}>
+                                        <img src={part.image} className="hardware_img" alt="hardware"/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import FirstScreen from "../../components/mianScreenBG/firstScreen";
 import FiveReasons from "./fiveReasons/fiveReasons";
+import Device from "./device/device";
 
 export default function Main(){
     return(
         <>
         <FirstScreen />
         <FiveReasons />
+        <Device />
         </>
     )
 }
