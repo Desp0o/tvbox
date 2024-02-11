f63d9cf04fb6f8f8b0ca6cb03cc59cf73cc056b4
Despo <tornike.despotashvili@gmail.com>
2024-02-11 14:37:26 +0400

    add lazyLoad to singleChannelComponent

--- src/components/singleChannelComponent/singleChannelComponent.jsx
+++ src/components/singleChannelComponent/singleChannelComponent.jsx
@@ -7,22 +7,22 @@ export default function SingleChannelComponent({isFaded, isAbroad, icon, name, t
         <div className={isFaded === true || isAbroad === false ? 'channel_list_item_faded' : 'channel_list_item'}>
             {/* არხის სახელი და იქონი */}
             <div className="channel_item_name_cover">
-                <img src={icon} alt='' className="channelLogo" loading="lazy"/>
+                <img loading="lazy" src={icon} alt='' className="channelLogo"/>
                 <p>{name}</p>
             </div>
 
             {/* თავსებადობა */}
             <div className="channel_item_devices">
                 <div className="device_little_block">
-                    {tv ? <img src={tv} alt="tv" className="tv_icon_class" /> : <></>}
+                    {tv ? <img loading="lazy" src={tv} alt="tv" className="tv_icon_class" /> : <></>}
                 </div>
 
                 <div className="device_little_block">
-                    {desktop ? <img src={desktop} alt="desktop" className="desktop_icon_class" /> : <></>}
+                    {desktop ? <img loading="lazy" src={desktop} alt="desktop" className="desktop_icon_class" /> : <></>}
                 </div>
 
                 <div className="device_little_block">
-                    {mobile ? <img src={mobile} alt="mobile" className="mobile_icon_class" /> : <></>}
+                    {mobile ? <img loading="lazy" src={mobile} alt="mobile" className="mobile_icon_class" /> : <></>}
                 </div>
             </div>
         </div>
